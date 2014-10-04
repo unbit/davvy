e3f3fd8ff029f3a4b35e986014fac7875ebd0d9e
Aldur <adrianodl@hotmail.it>
2014-10-04 15:46:26 +0200

    fix django deprecation warning

--- davvy/admin.py
+++ davvy/admin.py
@@ -4,22 +4,24 @@ from django import forms
 
 
 class PropInline(admin.TabularInline):
-    model = Prop
+	fields = ['resource', 'name']
+	model = Prop
 
 
 class ResourceAdminForm(forms.ModelForm):
 
-    class Meta:
-        model = Resource
-        widgets = {
-            'file': forms.TextInput(attrs={'size': '64'})
-        }
+	class Meta:
+		model = Resource
+		exclude = []
+		widgets = {
+			'file': forms.TextInput(attrs={'size': '64'})
+		}
 
 
 class ResourceAdmin(admin.ModelAdmin):
-    list_display = ('__unicode__', 'displayname', 'user')
-    form = ResourceAdminForm
-    inlines = [PropInline]
+	list_display = ('__unicode__', 'displayname', 'user')
+	form = ResourceAdminForm
+	inlines = [PropInline]
 
 
 admin.site.register(Resource, ResourceAdmin)
