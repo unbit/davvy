b3ad602545ca3a77ea88a84c22b3b2dfa6f288f3
Aldur <adrianodl@hotmail.it>
2014-10-15 13:30:49 +0200

    working Apple autodiscovery

--- davvy/wellknown.py
+++ davvy/wellknown.py
@@ -1,11 +1,11 @@
 from django.http import HttpResponse
 from django.conf import settings
-# from django.shortcuts import redirect
 from django.views.decorators.csrf import csrf_exempt
 from django.contrib.auth import authenticate, login
 from django.contrib.auth.models import User
 import base64
 
+import davvy
 from davvy.base import WebDAV
 
 import logging
@@ -31,6 +31,28 @@ class WellKnownDAV(WebDAV):
             login(request, user)
             request.user = user
 
+            # choose the correct current-user-principal handler (calendars/addressbooks)
+            if self.root == "calendars":
+                from davvy.calendar import prop_dav_calendar_home_set as prop_dav_resource_home_set
+            elif self.root == "addressbook001":
+                from davvy.addressbook import prop_dav_addressbook_home_set as prop_dav_resource_home_set
+            else:
+                prop_dav_resource_home_set = None
+
+            old_cup_prop = None
+            if prop_dav_resource_home_set:
+                # let's backup current-user-principal handler
+                cup = '{DAV:}current-user-principal'
+                old_cup_prop = davvy.retrieve_prop(
+                    cup
+                )
+
+                # let's modify it
+                davvy.register_prop(
+                    cup,
+                    prop_dav_resource_home_set,
+                    davvy.exceptions.Forbidden)
+
             try:
                 response = super(WebDAV, self).dispatch(
                     request, user.username, "/", *args, **kwargs
@@ -38,27 +60,18 @@ class WellKnownDAV(WebDAV):
                 dav_base = ['1']
                 dav_base += getattr(settings, 'DAVVY_EXTENSIONS', [])
                 response['Dav'] = ','.join(dav_base + self.dav_extensions)
-
-                # current_user_principal = getattr(
-                #     settings, 'DAVVY_CALENDAR_HOME_SET_BASE', None
-                # )
-
-                # def _dav_calendar_home_set(request):
-                #     if current_user_principal is not None:
-                #         if isinstance(current_user_principal, list) or isinstance(current_user_principal, tuple):
-                #             for base in current_user_principal:
-                #                 yield base.rstrip('/') + '/' + request.user.username + '/'
-                #         else:
-                #             yield current_user_principal.rstrip('/') + '/' + request.user.username + '/'
-
-                # redirect_url = list(_dav_calendar_home_set(request))[0]
-                # response = redirect(redirect_url, permanent=True)
             except Exception as e:
                 logger.debug(e)
                 code, phrase = e.status.split(' ', 1)
                 response = HttpResponse(phrase, content_type='text/plain')
                 response.status_code = int(code)
                 response.reason_phrase = phrase
+            finally:
+                if old_cup_prop:
+                    # restore current-user-principal
+                    davvy.register_prop(
+                        *old_cup_prop
+                    )
 
         else:
             response = HttpResponse('Unathorized', content_type='text/plain')
